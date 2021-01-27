b721a3be090f280c81160d0fbcda035e51da37fc
Joshua Lokken <jrrlokken@gmail.com>
2021-01-26 19:59:15 -0600

    Add deploy URL

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production"
-  ? 'https://mask-up.herokuapp.com'
+  ? 'https://mask-up.vercel.app'
   : 'http://localhost:3000';
 
   export default baseUrl;
